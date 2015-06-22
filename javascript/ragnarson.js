22bb605fe7b061dc9ef561a6bb981724f786dcaf
embargiel <marcin.k.bargiel@gmail.com>
2015-06-22 14:43:01 +0200

    interview

--- javascript/ragnarson.js
+++ javascript/ragnarson.js
@@ -11,7 +11,28 @@ $(document).ready(
     $('#background_div').click(function(){
       $('#user_form').fadeOut(400);
       $('#background_div').hide();
-    })
+    });
+
+    $(document).on('click', '#members_login', function(){
+      sortTable("members", "login");
+    });
+    $(document).on('click', '#invited_login', function(){
+      sortTable("invited", "login");
+    });
+    $(document).on('click', '#owners_login', function(){
+      sortTable("owners", "login");
+    });
+
+    $(document).on('click', '#members_email', function(){
+      sortTable("members", "email");
+    });
+    $(document).on('click', '#invited_email', function(){
+      sortTable("invited", "email");
+    });
+    $(document).on('click', '#owners_email', function(){
+      sortTable("owners", "email");
+    });
+
 
     $(document).on('click', '.button_remove', function(){
       var element = $(this).attr("id").split("_");      
@@ -37,37 +58,38 @@ $(document).ready(
       $.ajax({        
         type: "POST",
         data: $(this).serialize(),  
-        success: function () {
-          $('#user_form').hide();
+        success: function () {          
           var login = $('#username').val();
           var email = $('#email').val();
           var type = "members"
           if($('#radio_owner').prop('checked'))
           {           
             type = "owners"
-          } 
-          data["invited"].push( {"login":login, "email":email, "type":type});         
-          refreshCategory("invited");
-          $('#background_div').hide();
+          }           
+          data["invited"].push( {"login":login, "email":email, "type":type});
+          refreshCategory("invited");            
+          $('#background_div').hide();  
+          $('#user_form').hide();                      
         }           
       });
       ev.preventDefault();          
     });
 
-    function refreshCategory(category) {
-      $('#' + category + ' div').remove();
+    function refreshCategory(category) {      
+      $('#' + category + ' table' + ' tr').remove();
+      $('#' + category + "_table").append("<tr><td>id</td><td id='" + category + "_login'>login \\/</td><td id='" + category + "_email'>email \\/</td><td>remove</td><td>edit</td></tr>");
       for(var j = 0; j < data[category].length; j++)
       {
-        $('#' + category).append("<div></div>");
-        $('#' + category + ' div:last-child').text((data[category][j].login || "unknown") + " - email: " + (data[category][j].email || "unknown"));
-        $('#' + category + ' div:last-child').append('<i class="fa fa-times button_remove" id="' + category + '_' + j + '"><span class="hint">Remove</span></i>');
+        $('#' + category + "_table").append("<tr></tr>");
+        $('#' + category + '_table' + ' tr:last-child').append('<td>' + parseInt(j+1) + '</td><td>' + (data[category][j].login || "unknown") + '</td><td>' + (data[category][j].email || "unknown") + '</td>');
+        $('#' + category + '_table' + ' tr:last-child').append('<td><i class="fa fa-times button_remove" id="' + category + '_' + j + '"><span class="hint">Remove</span></i></td>');
         if(category == "members")
         {
-          $('#' + category + ' div:last-child').append('<i class="fa fa-pencil-square-o button_edit" id="edit_' + category + '_' + j + '"><span class="hint">Upgrade</span></i>');
+          $('#' + category + '_table' + ' tr:last-child').append('<td><i class="fa fa-pencil-square-o button_edit" id="edit_' + category + '_' + j + '"><span class="hint">Upgrade</span></i></td>');
         }
         else if(category == "owners")
         {
-          $('#' + category + ' div:last-child').append('<i class="fa fa-pencil-square-o button_edit" id="edit_' + category + '_' + j + '"><span class="hint">Downgrade</span></i>');
+          $('#' + category + '_table' + ' tr:last-child').append('<td><i class="fa fa-pencil-square-o button_edit" id="edit_' + category + '_' + j + '"><span class="hint">Downgrade</span></i></td>');
         }
       }
     };
@@ -76,6 +98,20 @@ $(document).ready(
       refreshCategory("members");
       refreshCategory("owners");
       refreshCategory("invited");
-    };    
+    }; 
+
+    function sortTable(category, field) {
+      data[category].sort(function(a, b){
+        var a1 = a[field];
+        var b1 = b[field];
+        if(a1 == b1){
+          return 0;
+        }
+        else{
+          return a1 > b1? 1: -1;
+        }
+      });
+      refreshCategory(category);
+    };   
   }
 );
